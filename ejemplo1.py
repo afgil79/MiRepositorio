24c6464e9c869f8385aea99c9753634b44588ec9
fedegil79 <afgil1979@gmail.com>
2023-03-05 12:01:01 -0300

    Agregar hola mundo Poo
    Editar Boton Ver y Mensaje

--- ejemplo1.py
+++ ejemplo1.py
@@ -5,6 +5,6 @@ from tkinter import ttk
 
 root = tk.Tk()
 frame = ttk.Frame(root).grid()
-ttk.Button(frame, text="Hola",
-           command=lambda: print("mundo")).grid()
+ttk.Button(frame, text="Ver",
+           command=lambda: print("Hola mundo!")).grid()
 root.mainloop()
