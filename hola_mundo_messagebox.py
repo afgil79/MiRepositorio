31de10b4ca60fc492708fd07b1af7d3eae0bfe2d
fedegil79 <afgil1979@gmail.com>
2023-03-05 14:32:09 -0300

    Crear Hola Mundo con MsgBox

---
+++ hola_mundo_messagebox.py
@@ -0,0 +1,17 @@
+# hola_mundo_messagebox.py
+
+import tkinter as tk
+from tkinter import ttk, messagebox
+
+class App(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        ttk.Button(self, text="Hola", command=self.mensaje).grid()
+
+    @staticmethod
+    def mensaje(msj='mundo!'):
+        messagebox.showinfo(message=msj)
+
+root = tk.Tk()
+App(root).grid()
+root.mainloop()
